5b70412afc8155121530aee1cceca72ac75be018
pierre <oprbguitar@gmail.com>
2013-05-23 19:36:44 -0500

    sesion2
    clase de java web

---
+++ clase 02/session2/src/java/Aplicaciones/Ejemplo1.java
@@ -0,0 +1,30 @@
+package Aplicaciones;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Ejemplo1 extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+      }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
